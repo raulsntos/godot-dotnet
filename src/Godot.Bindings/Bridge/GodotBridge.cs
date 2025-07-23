c1af80a0640d4c3542dcae3d16118c0b84592c7b
Paul Joannon <hello@pauljoannon.com>
2025-07-23 20:07:11 +0200

    Implement GodotSynchronizationContext

--- src/Godot.Bindings/Bridge/GodotBridge.cs
+++ src/Godot.Bindings/Bridge/GodotBridge.cs
@@ -78,6 +78,15 @@ public static partial class GodotBridge
             minimum_initialization_level = (GDExtensionInitializationLevel)configuration.MinimumInitLevel,
         };
 
+        GodotSynchronizationContext.InitializeSynchronizationContext();
+
+        var mainLoopCallbacks = new GDExtensionMainLoopCallbacks()
+        {
+            frame_func = (nint)(delegate* unmanaged[Cdecl]<void>)(&Frame_Native)
+        };
+
+        _gdextensionInterface.register_main_loop_callbacks(_libraryPtr, &mainLoopCallbacks);
+
         _initialized = true;
     }
 
@@ -135,5 +144,11 @@ public static partial class GodotBridge
 
             DisposablesTracker.DisposeAll();
         }
+    }
+
+    [UnmanagedCallersOnly(CallConvs = [typeof(CallConvCdecl)])]
+    private static void Frame_Native()
+    {
+        GodotSynchronizationContext.ExecutePendingContinuations();
     }
 }
---
+++ src/Godot.Bindings/Core/GodotSynchronizationContext.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Godot;
+
+internal sealed class GodotSynchronizationContext : SynchronizationContext, IDisposable
+{
+    private readonly BlockingCollection<(SendOrPostCallback Callback, object? State)> _queue = [];
+
+    private GodotSynchronizationContext()
+    {
+    }
+
+    /// <inheritdoc />
+    public override void Send(SendOrPostCallback d, object? state)
+    {
+        // If we're already processing on the main thread, we can invoke directly.
+        if (SynchronizationContext.Current == this)
+        {
+            d(state);
+            return;
+        }
+
+        var completion = new TaskCompletionSource();
+
+        _queue.Add((
+            Callback: state =>
+            {
+                try
+                {
+                    d(state);
+                }
+                finally
+                {
+                    completion.SetResult();
+                }
+            },
+            State: state
+        ));
+
+        completion.Task.Wait();
+    }
+
+    /// <inheritdoc />
+    public override void Post(SendOrPostCallback d, object? state)
+    {
+        _queue.Add((Callback: d, State: state));
+    }
+
+    /// <inheritdoc cref="ExecutePendingContinuations"/>
+    private void ExecutePendingContinuationsCore()
+    {
+        while (_queue.TryTake(out var workItem))
+        {
+            workItem.Callback.Invoke(workItem.State);
+        }
+    }
+
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        _queue.Dispose();
+    }
+
+    /// <summary>
+    /// Initializes the synchronization context. This should be called before any user code is executed.
+    /// </summary>
+    internal static void InitializeSynchronizationContext()
+    {
+        SynchronizationContext.SetSynchronizationContext(new GodotSynchronizationContext());
+    }
+
+    /// <summary>
+    /// Executes all queued continuations.
+    /// </summary>
+    internal static void ExecutePendingContinuations()
+    {
+        if (SynchronizationContext.Current is not GodotSynchronizationContext context)
+        {
+            return;
+        }
+
+        context.ExecutePendingContinuationsCore();
+    }
+}
