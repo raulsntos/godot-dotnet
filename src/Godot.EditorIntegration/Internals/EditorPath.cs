186488a126b79e21715a49a7219e940e64da804d
Paul Joannon <hello@pauljoannon.com>
2025-11-17 23:24:51 +0100

    [WIP] Support slnx

--- src/Godot.EditorIntegration/Internals/EditorPath.cs
+++ src/Godot.EditorIntegration/Internals/EditorPath.cs
@@ -1,7 +1,12 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
+using Microsoft.VisualStudio.SolutionPersistence;
+using Microsoft.VisualStudio.SolutionPersistence.Model;
+using Microsoft.VisualStudio.SolutionPersistence.Serializer;
 
 namespace Godot.EditorIntegration.Internals;
 
@@ -18,7 +23,7 @@ internal static class EditorPath
 
     public static string ProjectAssemblyName => _projectAssemblyName ??= EditorInternal.GetProjectAssemblyName();
 
-    public static string ProjectSlnPath => _slnPath ??= EditorInternal.GetProjectSlnPath();
+    public static string ProjectSlnPath => _slnPath ??= GetProjectSlnOrSlnxPath();
 
     public static string ProjectCSProjPath => _csprojPath ??= EditorInternal.GetProjectCSProjPath();
 
@@ -42,5 +47,61 @@ internal static class EditorPath
         _projectAssemblyName = null;
         _slnPath = null;
         _csprojPath = null;
+    }
+
+    private static string GetProjectSlnOrSlnxPath()
+    {
+        string slnPath = EditorInternal.GetProjectSlnPath();
+        string slnDir = Path.GetDirectoryName(slnPath)!;
+
+        List<string> slnPaths =
+        [
+            .. Directory.GetFiles(slnDir, "*.sln"),
+            .. Directory.GetFiles(slnDir, "*.slnx"),
+        ];
+
+        for (int i = slnPaths.Count - 1; i > 0; --i)
+        {
+            ISolutionSerializer? serializer = SolutionSerializers.GetSerializerByMoniker(slnPaths[i]);
+
+            if (serializer is null)
+            {
+                goto SolutionIsInvalid;
+            }
+
+            SolutionModel solution = serializer.OpenAsync(slnPaths[i], CancellationToken.None).Result;
+
+            foreach (SolutionProjectModel project in solution.SolutionProjects)
+            {
+                string csProjPath = Path.GetFullPath(project.FilePath, Path.GetDirectoryName(slnPaths[i])!)
+                                        .Replace('\\', '/');
+
+                if (string.Equals(csProjPath, ProjectCSProjPath, StringComparison.Ordinal))
+                {
+                    goto SolutionIsValid;
+                }
+            }
+
+        SolutionIsInvalid:
+            slnPaths.RemoveAt(i);
+        SolutionIsValid:;
+        }
+
+        // TODO: Better error handling.
+        switch (slnPaths.Count)
+        {
+            case 0:
+                return slnPath;
+            case 1:
+                return slnPaths[0];
+            default:
+                GD.PushError($"""
+                    Multiple solutions containing a project with assembly name '{ProjectAssemblyName}' were found:
+                    {string.Join('\n', slnPaths).Replace('\\', '/')}
+                    Please ensure only one solution contains the project assembly.
+                    If you have recently migrated to .slnx please ensure that you have removed the unused .sln.
+                    """);
+                return string.Empty;
+        }
     }
 }
